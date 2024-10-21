0b173f3ed4422368de986ffbb4aee3a785fed2d0
seongun Kim <seongun44@naver.com>
2024-10-21 23:00:04 +0900

    :sparkles: Feat : Item, CartItem 개발.
    1. Feat: Cart Repository 추가
    2. Feat: CartItem 엔티티 추가
    : 상품과 장바구니는 n:m 관계이므로, 이를 해소할 장바구니상품(CartItem)이 필요함.
    3. Feat: CartController, CartItemController 추가.
    
    4. Item 속성 Count int로 변경.
    : 기본 타입으로 변환하여 계산하는 로직이 필요해서 (Long -> item으로 변경.)
    5. 사소한 버그 수정

--- src/main/java/com/example/codingmall/Cart/Cart.java
+++ src/main/java/com/example/codingmall/Cart/Cart.java
@@ -1,14 +1,17 @@
 package com.example.codingmall.Cart;
 
-import com.example.codingmall.Item.Item;
+import com.example.codingmall.CartItem.CartItem;
 import com.example.codingmall.User.User;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
+import java.util.List;
 @Entity
 @Getter
+@NoArgsConstructor
 public class Cart {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cart_id")
@@ -18,12 +21,24 @@ public class Cart {
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id",nullable = false)
-    private Item item;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
 
     private int count; // 상품 수량
     private int price; // 상품 가격
     private LocalDateTime registerDate;// 등록일시
     private LocalDateTime updateDate; //수정일시
+
+    public  Cart (User user){
+        this.user = user;
+        this.registerDate = LocalDateTime.now();
+    }
+    public void addItem(CartItem cartItem){
+        items.add(cartItem);
+        cartItem.setCart(this);
+        this.updateDate = LocalDateTime.now();
+    }
+
 }
