1f767c8b0b5817c3ad9ab8a31c14516501ac7f34
Dev-SSW <sgg919@naver.com>
2024-11-26 17:18:40 +0900

    ♻️ Refactor : SignupRequest에 필요한 입력 값만을 받을 수 있도록 수정했습니다.
    1. username,password,birth,name,phoneNumber을 제외한 모든 입력 삭제
    2. 입력을 삭제하고 가입 시 자동으로 ROLE_USER, ACTIVATE를 부여받도록 수정
    3. USER의 jumin을 birth로 수정
    4. ROLE에 SOCIAL 역할 추가 (USER, SOCIAL, ADMIN)
    5. SOCIAL 역할 사용자도 가입한 사용자 용 url을 들어갈 수 있도록 hasAnyRole로 수정
    6. 소셜 로그인 한 회원의 생년월일과 휴대전화 번호를 가져오는 로직은 수정중
    7. 추후 어드민 계정을 수동으로 추가하는 기능을 만들 예정

--- src/main/java/com/example/codingmall/User/Login/LoginDto/SignupRequest.java
+++ src/main/java/com/example/codingmall/User/Login/LoginDto/SignupRequest.java
@@ -13,12 +13,7 @@ import lombok.Data;
 public class SignupRequest {
     private String username;
     private String password;
-    private int jumin;       // 주민등록번호
+    private int birth;       // 생년월일
     private String name;     // 사용자 이름
-    private String email;    // 이메일
     private String phoneNumber;// 휴대폰 번호
-    @Schema(description = "유저 상태(탈퇴 회원 여부 파악)",defaultValue = "ACTIVATE",allowableValues = {"ACTIVATE","DEACTIVATE"})
-    private UserStatus status;   // 상태(탈퇴 회원 여부 파악)
-    @Schema(description = "유저 역할",defaultValue = "ROLE_USER",allowableValues = {"ROLE_USER","ROLE_ADMIN"})
-    private Role role;       // 역할
 }
