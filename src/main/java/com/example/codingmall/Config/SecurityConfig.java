ec5e05d2952325d27f515ef63e118059cf353c20
Dev-SSW <sgg919@naver.com>
2024-12-08 13:01:21 +0900

    ♻️ Refactor : 요청에 인증되지 않은 사용자가 입력됐을 때, 401 코드를 출력하여 관련 오류를 관리하기 위해 리팩토링했습니다.

--- src/main/java/com/example/codingmall/Config/SecurityConfig.java
+++ src/main/java/com/example/codingmall/Config/SecurityConfig.java
@@ -4,9 +4,12 @@ import com.example.codingmall.User.Login.Jwt.JWTAuthFilter;
 import com.example.codingmall.User.Login.OAuth2.CustomOAuth2UserService;
 import com.example.codingmall.User.Login.OAuth2.CustomSuccessHandler;
 import com.example.codingmall.User.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,6 +21,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -49,13 +53,15 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  //OPTIONS 요청(Preflight)에 대한 명시적인 허용
                         .requestMatchers(
+                                "/public/**",
+                                "/error",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs/swagger-config",
                                 "/v3/api-docs.yaml"
                         ).permitAll()
-                        .requestMatchers("/public/**", "/error").permitAll()
+                        .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN") // ADMIN 역할을 가진 사용자만 접근 허용
                         .requestMatchers("/user/**").hasAnyRole("USER", "SOCIAL") // USER 역할을 가진 사용자만 접근 허용
                         .anyRequest().authenticated())
@@ -63,6 +69,8 @@ public class SecurityConfig {
                         .successHandler(customSuccessHandler)
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint())) // AuthenticationEntryPoint 설정
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
@@ -85,11 +93,20 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean /* 인증 없는 사용자 오류 발생 시 /login 리다이렉트 대신 401 오류를 반환하도록 추가한 예외 코드 */
+    public AuthenticationEntryPoint customAuthenticationEntryPoint() {
+        return (HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 반환
+            response.getWriter().write("Unauthorized: " + authException.getMessage());
+        };
+    }
+
     // CORS 설정을 위한 CorsConfigurationSource 빈 생성
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://*.ngrok-free.app", "http://localhost:3000")); // ngrok 도메인 허용, 리액트 도메인 허용
+        configuration.setAllowedOriginPatterns(List.of("https://*.ngrok-free.app"/*, "http://localhost:3000"*/)); // ngrok 도메인 허용, 리액트 도메인 허용
+                                                                                                                  // *:와일드 카드 URL을 사용하려면 OriginPatterns를 사용해야 합니다.
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
