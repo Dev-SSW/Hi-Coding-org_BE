64c8cf24ccbef611fa320b081dd0c328e62a1297
Dev-SSW <sgg919@naver.com>
2025-04-28 14:48:01 +0900

    ✨ Feat: 오류 코드 전반적 관리 기능을 추가, DeviceService 오류 코드 수정
    1. 오류 코드를 전체적으로 관리할 수 있도록 파일을 작성했습니다.
    2. DeviceService에 대한 오류들은 (code, Message, custom error code) 형식으로 출력됩니다

---
+++ src/main/java/com/example/codingmall/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.codingmall.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SerialNumberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleSerialNumberNotFound(SerialNumberNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), "SERIAL_NUMBER_NOT_FOUND"));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), "USER_NOT_FOUND"));
+    }
+
+    @ExceptionHandler(AlreadyHasPlantRoleException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyHasRole(AlreadyHasPlantRoleException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), "ALREADY_PLANT_ROLE"));
+    }
+}
