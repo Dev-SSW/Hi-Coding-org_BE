8040962a5e54d4045c45bea41ee83a5a59d1f3b6
Dev-SSW <sgg919@naver.com>
2025-05-03 17:03:39 +0900

    🐛 Fix: PlantGrowthLogRequest의 record를 Datetime.now()에서 직접 설정하도록 변경
    1. PlantGrowthLog의 기록된 날짜를 입력하는 란을 추가하여, 기록 생성 시 날짜도 지정할 수 있도록 수정하였습니다.
    2. 그에 따라 이전 날짜 기록을 수정하려고 할 시, 기록이 있다면 덮어쓰도록 하고,기록이 없다면 새로 생성하도록 수정했습니다.
    3. 기록 수정 시 식물 길이를 입력하지 않았다면 기존의 식물 길이를 가져옵니다. 그렇지 않다면 새롭게 작성된 식물 길이로 수정합니다
    4. 내용의 경우는 항상 써져있는 그대로 가져옵니다.

--- src/main/java/com/example/codingmall/PlantGrowthLog/PlantGrowthLogService.java
+++ src/main/java/com/example/codingmall/PlantGrowthLog/PlantGrowthLogService.java
@@ -22,21 +22,45 @@ public class PlantGrowthLogService {
     /* 성장 기록 저장 */
     @Transactional
     public Long recordGrowth(Long plantId, PlantGrowthLogRequest plantGrowthLogRequest) {
-        Plant plant = plantRepository.findById(plantId).orElseThrow(() -> new PlantNotFoundException("해당 식물을 찾을 수 없습니다."));
-        int totalGrowth = plantGrowthLogRequest.getGrowth() + plant.getTotalGrowth();   //전체 성장 길이 Plant에 저장
-        plant.setTotalGrowth(totalGrowth);
-        plantRepository.save(plant);
+        // 1. 해당 식물 찾기
+        Plant plant = plantRepository.findById(plantId)
+                .orElseThrow(() -> new PlantNotFoundException("해당 식물을 찾을 수 없습니다."));
+        // 2. 해당 날짜에 일기가 있는지 확인
+        LocalDate recordDate = plantGrowthLogRequest.getRecord();
+        PlantGrowthLog existLog = plantGrowthLogRepository
+                .findFirstByPlantIdAndRecord(plantId, recordDate)
+                .orElse(null);
+        // 3. 일기가 없다면 새로 생성
+        if (existLog == null) {
+            int totalGrowth = plantGrowthLogRequest.getGrowth() + plant.getTotalGrowth();   //입력된 성장 길이 + 기존 식물의 성장 길이
+            plant.updateGrowth(totalGrowth);                                                //식물의 성장 길이를 수정 (변경 감지로 수정)
+            PlantGrowthLog growthLog = PlantGrowthLog.createLog(plant, plantGrowthLogRequest);
+            plantGrowthLogRepository.save(growthLog);
+            return growthLog.getId();
+        }
+        // 4. 기록이 있다면 덮어쓰기
+        int originTotalGrowth = plant.getTotalGrowth();     //기존 저장 기록 시의 식물 전체 길이
+        int originGrowth = existLog.getGrowth();            //기존 저장 기록의 성장 길이
 
-        int percentage = (int) ((double) totalGrowth / plant.getGrowthTarget() * 100); //성장률 계산
-        PlantGrowthLog growthLog = PlantGrowthLog.createLog(plant, plantGrowthLogRequest, percentage, totalGrowth);
-        plantGrowthLogRepository.save(growthLog);
-        return growthLog.getId();
+        int finalGrowth;
+        if (plantGrowthLogRequest.getGrowth() == 0) {       // 성장 길이 입력 값이 0이라면
+            finalGrowth = originGrowth;                     // 기존 저장 기록의 성장 길이를 저장
+        } else {                                            // 성장 길이 입력 값이 0이 아니라면
+            finalGrowth = plantGrowthLogRequest.getGrowth();// 성장 길이 입력 값을 저장
+        }
+        int finalTotalGrowth = originTotalGrowth - originGrowth + finalGrowth;               // 전체 성장 길이 다시 구하기
+        plant.updateGrowth(finalTotalGrowth);
+
+        String finalContent = plantGrowthLogRequest.getContent();
+        existLog.updateLog(finalGrowth, finalContent, existLog.getRecord());
+        return existLog.getId();
     }
 
 
     /* 특정 날짜의 성장 기록 단일 조회 */
     public PlantGrowthLog getGrowthLogByDate(Long plantId, LocalDate date) {
-        return plantGrowthLogRepository.findFirstByPlantIdAndRecord(plantId, date).orElseThrow(() -> new PlantLogsNotFoundByDate("해당 날짜의 성장 기록이 없습니다."));
+        return plantGrowthLogRepository.findFirstByPlantIdAndRecord(plantId, date)
+                .orElseThrow(() -> new PlantLogsNotFoundByDate("해당 날짜의 성장 기록이 없습니다."));
     }
 
     public List<PlantGrowthLog> getAllGrowthLogs(Long plantId) {
